77c6a5927c6d11a3e7db2e521f3810b03ddf369c
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:12:05 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -10,16 +10,14 @@ for i in [
     "load_fm.py",
     "load_kb.py",
     "load_lock.py",
-    "load_settings.py"
-    "unload_fm.py",
+    "load_settings.py" "unload_fm.py",
     "unload_kb.py",
     "unload_lock.py",
-    "unload_settings.py"
-    "womp.py",
+    "unload_settings.py" "womp.py",
 ]:
-    shutil.copy(i, path.join(root, "bin/womp", i))
+    shutil.copyfile(i, path.join(root, "bin/womp", i))
 
-shutil.copy("womp.lja", path.join(root, "bin", "womp.lja"))
+shutil.copyfile("womp.lja", path.join(root, "bin", "womp.lja"))
 
 try:
     mkdir(path.join(root, "boot"))
@@ -31,7 +29,7 @@ try:
 except FileExistsError:
     pass
 
-shutil.copy("99-womp.lja", path.join(root, "boot/boot.d", "99-womp.lja"))
+shutil.copyfile("99-womp.lja", path.join(root, "boot/boot.d", "99-womp.lja"))
 
 try:
     mkdir(path.join(root, "usr/share/applications"))
